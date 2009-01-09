2ffab4d2d1355718229ca00e9f18dd04fd10aeb3
magi <magi@3966be30-755a-0410-815e-dd79f492ac3c>
2009-01-09 13:42:03 +0000

    Creation tag 7.1.4
    git-svn-id: https://outlook-client.forge.funambol.org/svn/client-sdk/tags/cpp-sdk@286 3966be30-755a-0410-815e-dd79f492ac3c

---
+++ 7.1.4/src/cpp/common/base/util/PropertyFile.cpp
@@ -0,0 +1,255 @@
+/*
+ * Funambol is a mobile platform developed by Funambol, Inc. 
+ * Copyright (C) 2003 - 2007 Funambol, Inc.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation with the addition of the following permission 
+ * added to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED
+ * WORK IN WHICH THE COPYRIGHT IS OWNED BY FUNAMBOL, FUNAMBOL DISCLAIMS THE 
+ * WARRANTY OF NON INFRINGEMENT  OF THIRD PARTY RIGHTS.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License 
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
+ * 
+ * You can contact Funambol, Inc. headquarters at 643 Bair Island Road, Suite 
+ * 305, Redwood City, CA 94063, USA, or at email address info@funambol.com.
+ * 
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ * 
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * "Powered by Funambol" logo. If the display of the logo is not reasonably 
+ * feasible for technical reasons, the Appropriate Legal Notices must display
+ * the words "Powered by Funambol".
+ */
+
+#include "base/util/PropertyFile.h"
+#include "base/globalsdef.h"
+
+#define REMOVED "__#REM#O#VED#__"
+
+USE_NAMESPACE
+
+
+StringBuffer escapeString(const char* val) {
+    StringBuffer s(val);
+    s.trim();
+    s.replaceAll("\\", "\\\\");
+    s.replaceAll("=", "\\=");
+    return s;
+}
+
+StringBuffer unescapeString(const char* val) {
+    StringBuffer s(val);
+    s.trim();
+    s.replaceAll("\\=", "=");
+    s.replaceAll("\\\\", "\\");
+    return s;
+}
+
+static bool existsFile(const char* fullname) {
+    bool found = false;
+    FILE* f = fopen(fullname, "r");
+    if (f) {
+        found = true;
+        fclose(f);
+    }
+    return found;
+}
+
+static bool removeFile(const char* fullname) {
+    char* p;
+	int len;
+    bool ret = false;
+    
+    // The path separator could be '/' or '\'
+    p = strrchr((char*)fullname, '/');
+    if (!p) {
+        // try with '\'
+        p = strrchr((char*)fullname, '\\');
+    }
+
+    if (!p) {
+        // the file is in the current directory
+        ret = removeFileInDir(".", fullname);                
+    } else {
+	    len = p-fullname;        
+        StringBuffer dir(fullname, len);	 
+	    p++; len=strlen(fullname)-len;
+        StringBuffer filename(p, len);
+        ret = removeFileInDir(dir, filename);
+    }    	
+    return ret;
+}
+
+
+int PropertyFile::read() {
+    
+    char line[512];
+    FILE* f;
+    f = fopen(node, "r");
+    if (!f) {
+        //LOG.debug("PropertyFile: the file '%s' doesn't exist. Try the journal file '%s'", node.c_str(), nodeJour.c_str());        
+    } else {
+        while(fgets(line, 511, f) != NULL) {
+            StringBuffer s(line);
+            StringBuffer key;
+            StringBuffer value;
+
+            if (separateKeyValue(s, key, value)) {
+                KeyValuePair toInsert(key, value);
+                data.add(toInsert);
+            }            
+        }   
+        fclose(f);   
+    }
+    // check if there is the journal file and if any, set every value in memory. After that
+    // empty the journal
+    f = fopen(nodeJour, "r");
+    if (!f) {
+        // LOG.debug("PropertyFile: there is no journal file: '%s'", nodeJour.c_str());        
+    } else {
+        while(fgets(line, 511, f) != NULL) {
+            StringBuffer s(line);
+            StringBuffer key;
+            StringBuffer value;
+            if (separateKeyValue(s, key, value)) {
+                if (value == REMOVED) {
+                    MemoryKeyValueStore::removeProperty(key);
+                } else {
+                    MemoryKeyValueStore::setPropertyValue(key, value);
+                }
+            }                  
+        }       
+        fclose(f);         
+    }
+    return 0;
+}
+
+int PropertyFile::close() {
+
+    FILE* file;
+    file = fopen(node, "w");
+    int ret = 0;   
+    if (file) {
+        KeyValuePair* curr = NULL;         
+        for (curr = (KeyValuePair*)data.front(); curr;
+             curr = (KeyValuePair*)data.next() ) {
+            
+             fprintf(file, "%s=%s\n", escapeString(curr->getKey()).c_str(), escapeString(curr->getValue()).c_str());            
+        }       
+        fclose(file); 
+        
+        // reset the content of the journal        
+        if (existsFile(nodeJour) && !removeFile(nodeJour)) {
+            LOG.error("There are problem in removing journal file");
+        }
+        
+        ret = 0;
+
+    } else {
+        LOG.error("PropertyFile: it is not possible to save the file: '%s'", node.c_str());
+        ret = -1;
+    }
+    return ret;
+}
+
+int PropertyFile::setPropertyValue(const char *prop, const char *value) {
+    
+    StringBuffer p(prop), v(value);
+    p.trim();
+    v.trim();
+
+    int ret = MemoryKeyValueStore::setPropertyValue(p.c_str(), v.c_str());
+    if (ret) {
+        return ret;
+    }
+
+    FILE* file = fopen(nodeJour, "a+");    
+    if (file) {
+        fprintf(file, "%s=%s\n", escapeString(prop).c_str(), escapeString(value).c_str());
+        fclose(file);
+    } else {        
+        LOG.error("PropertyFile setProperty: it is not possible to save the journal file: '%s'", node.c_str());
+        ret = -1;
+    }
+
+    return ret;
+
+}
+
+int PropertyFile::removeProperty(const char *prop) {
+    
+    int ret = 0;
+
+    FILE* file = fopen(nodeJour, "a+");    
+    if (file) {
+        fprintf(file, "%s=%s\n", escapeString(prop).c_str(), escapeString(REMOVED).c_str());
+        fclose(file);
+    } else {        
+        LOG.error("PropertyFile removeProperty: it is not possible to save the journal file: '%s'", node.c_str());        
+    }
+    
+    StringBuffer p(prop); p.trim();
+    ret = MemoryKeyValueStore::removeProperty(p);
+    if (ret) {
+        LOG.debug("PropertyFile: it is not possible to remove from the ArrayList");
+    }
+
+    return ret;
+
+}
+
+int PropertyFile::removeAllProperties() {
+    
+    int ret = MemoryKeyValueStore::removeAllProperties();
+    if (ret) {
+        return ret;
+    }
+    // reset the content         
+    if (existsFile(node) && !removeFile(node)) {
+        LOG.error("There are problem in removing the file %s", node.c_str());
+    }            
+    return ret;
+}
+
+
+bool PropertyFile::separateKeyValue(StringBuffer& s, StringBuffer& key, StringBuffer& value) {
+    bool ret = false;        
+    bool foundSlash = false;
+    
+    for (unsigned int i = 0; i < s.length(); i++) {
+        
+        if (s.c_str()[i] == '\\') {
+            if (foundSlash) {
+                foundSlash = false;
+            } else {
+                foundSlash = true;
+            }
+            continue;
+        }
+        if (s.c_str()[i] == '=') {
+            if (foundSlash) {
+                foundSlash = false;
+                continue;
+            } else {
+                key = unescapeString(s.substr(0, i));
+                value = unescapeString(s.substr(i + 1, (s.length() - (i + 2)))); // it remove the \n at the end                                   
+                ret = true;
+                break;
+            }
+        }
+        
+    }
+    return ret;
+}
---
+++ 7.1.4/src/cpp/symbian/base/Log.cpp
@@ -0,0 +1,413 @@
+/*
+ * Funambol is a mobile platform developed by Funambol, Inc.
+ * Copyright (C) 2003 - 2007 Funambol, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation with the addition of the following permission
+ * added to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED
+ * WORK IN WHICH THE COPYRIGHT IS OWNED BY FUNAMBOL, FUNAMBOL DISCLAIMS THE
+ * WARRANTY OF NON INFRINGEMENT  OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
+ *
+ * You can contact Funambol, Inc. headquarters at 643 Bair Island Road, Suite
+ * 305, Redwood City, CA 94063, USA, or at email address info@funambol.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * "Powered by Funambol" logo. If the display of the logo is not reasonably
+ * feasible for technical reasons, the Appropriate Legal Notices must display
+ * the words "Powered by Funambol".
+ */
+
+#include <eikenv.h>
+#include <e32cmn.h>
+#include <bautils.h>
+#include "base/SymbianLog.h"
+#include "base/util/symbianUtils.h"
+#include "base/util/stringUtils.h"
+#include "base/util/timeUtils.h"
+#include "base/globalsdef.h"
+
+USE_NAMESPACE
+
+
+// Formats for date&time printed into log.
+_LIT(KFormatDateAndTime, "%*D%*N%Y%1-%2-%3 %:0%J%:1%T%:2%S");
+_LIT(KFormatOnlyTime,    "%:0%J%:1%T%:2%S");
+
+// The name of the semaphore
+_LIT(KLogSemaphoreName,  "FLogSemaphore");
+
+
+SymbianLog::SymbianLog(bool resetLog, const char* path, const char* name) 
+{
+    TInt err = KErrNone;
+    
+    const char* p = (path)? path : SYMBIAN_LOG_PATH;
+    const char* n = (name)? name : SYMBIAN_LOG_NAME;
+    
+    // assign all the paths and names
+    StringBuffer logName(p);
+    logName += n;
+    iLogName.Assign(charToNewBuf(logName.c_str()));
+    
+    StringBuffer rollName(logName);
+    rollName += ".0";
+    iRollLogName.Assign(charToNewBuf(rollName.c_str()));
+    
+    StringBuffer pathSb(p);
+    iLogPathName.Assign(charToNewBuf(pathSb.c_str()));
+    
+    StringBuffer nameSb(n);
+    iLogFileName.Assign(charToNewBuf(nameSb.c_str()));
+    
+    
+    // Create a semaphore, to avoid accessing the FileSystem at
+    // the same time by different threads.
+    // The semaphore is global, so that it could be used also by
+    // other processes that (in future) will use this Log.
+    err = iSemaphore.CreateGlobal(KLogSemaphoreName, 1);
+    if (err != KErrNone) {
+        setError(ERR_SEMAPHORE_CREATION, ERR_SEMAPHORE_CREATION_MSG);
+    }
+    iSemaphore.Wait();
+
+    
+    // Connect to the file server session.
+    fsSession.Connect();
+    err = fsSession.ShareAuto();
+    if (err != KErrNone) {
+        setErrorF(err, "SymbianLog error: unable to share RFs session (code %d)", err);
+        return;
+    }
+    
+    // ensure path exists!
+    BaflUtils::EnsurePathExistsL(fsSession, iLogPathName);
+    
+    if (resetLog) {
+        err = file.Replace(fsSession, iLogName, EFileWrite|EFileShareAny);
+        if (err != KErrNone) {
+            setErrorF(err, "SymbianLog: could not open the log file '%ls'", iLogName.Ptr());
+            return;
+        }
+        
+        // Write the Header
+        StringBuffer header = createHeader();
+        RBuf8 data;
+        data.Assign(stringBufferToNewBuf8(header));
+        file.Write(data);
+        data.Close();
+        file.Close();
+    }
+    
+    iSemaphore.Signal();
+    return;
+}
+
+SymbianLog::~SymbianLog() {
+    iLogPathName.Close();
+    iLogFileName.Close();
+    iLogName.Close();
+    iRollLogName.Close();
+}
+
+void SymbianLog::setLogPath(const char* configLogPath ) 
+{
+    if (configLogPath == NULL)
+    {
+        return;
+    }
+    iSemaphore.Wait();
+    
+    iLogPathName.Close();
+    iLogPathName.Assign(charToNewBuf(configLogPath));
+    
+    TInt size;
+    size = iLogPathName.Size() + iLogFileName.Size();
+    iLogName.Close();
+    iLogName.CreateL(size);
+    iLogName.Copy(iLogPathName);
+    iLogName.Append(iLogFileName);
+
+    iRollLogName.Close();
+    iRollLogName.CreateL(iLogName.Size());
+    iRollLogName.Copy(iLogName);
+    iRollLogName.Append(_L(".0"));
+    
+    // ensure path exists!
+    BaflUtils::EnsurePathExistsL(fsSession,iLogPathName);
+    
+    iSemaphore.Signal();
+    return;
+}
+
+void SymbianLog::setLogName(const char* configLogName ) 
+{
+    if (configLogName == NULL)
+    {
+        return;
+    }
+    iSemaphore.Wait();
+    
+    iLogFileName.Close();
+    iLogFileName.Assign(charToNewBuf(configLogName));
+    
+    TInt size;
+    size = iLogPathName.Size() + iLogFileName.Size();
+    iLogName.Close();
+    iLogName.CreateL(size);
+    iLogName.Copy(iLogPathName);
+    iLogName.Append(iLogFileName);
+
+    iRollLogName.Close();
+    iRollLogName.CreateL(iLogName.Size());
+    iRollLogName.Copy(iLogName);
+    iRollLogName.Append(_L(".0"));
+    
+    iSemaphore.Signal();
+    return;
+}
+
+
+StringBuffer SymbianLog::createHeader(const char* title) 
+{
+    const char *t = (title)? title : SYMBIAN_LOG_HEADER;
+    
+    StringBuffer header = createCurrentTime(true);      // e.g. "2008/04/02 10:58:03 GMT +2:00"
+    header += " - ";
+    header += t;
+    header += "\n";
+
+    return header;
+}
+
+StringBuffer SymbianLog::createCurrentTime(bool complete) 
+{
+    if (iFormattedBias.length() == 0) {
+        // Create the string e.g. "GMT +2:00" only once (it's always the same)
+        createFormattedBias();
+    }
+    
+    TTime local;
+    StringBuffer ret;
+    TBuf<50> formattedTime;
+    
+    local.HomeTime();
+
+    if (complete) { 
+        local.FormatL(formattedTime, KFormatDateAndTime); 
+        StringBuffer date = bufToStringBuffer(formattedTime);
+        ret.sprintf("%s %s", date.c_str(), iFormattedBias.c_str());
+    }
+    else { 
+        local.FormatL(formattedTime, KFormatOnlyTime);
+        ret = bufToStringBuffer(formattedTime);
+    }
+    return ret;
+}
+
+
+void SymbianLog::createFormattedBias() 
+{
+    TTime local, utc;
+    TTimeIntervalMinutes bias;
+    
+    local.HomeTime();
+    utc.UniversalTime();
+    local.MinutesFrom(utc, bias);
+
+    TInt totalMinutes = bias.Int();
+    TInt hours   = totalMinutes / 60;
+    TInt minutes = totalMinutes % 60;
+    
+    if (totalMinutes >= 0) { iFormattedBias.sprintf("GMT +%d:%02d", hours, minutes); }
+    else                   { iFormattedBias.sprintf("GMT %d:%02d",  hours, minutes); }
+}
+
+
+
+void SymbianLog::error(const char*  msg, ...) 
+{
+    PLATFORM_VA_LIST argList;
+    PLATFORM_VA_START (argList, msg);
+    printMessage(LOG_ERROR, msg, argList);
+    PLATFORM_VA_END(argList);
+}
+void SymbianLog::info (const char*  msg, ...) 
+{
+    if (isLoggable(LOG_LEVEL_INFO)) {
+        PLATFORM_VA_LIST argList;
+        PLATFORM_VA_START (argList, msg);
+        printMessage(LOG_INFO, msg, argList);
+        PLATFORM_VA_END(argList);
+    }
+}
+void SymbianLog::debug(const char*  msg, ...) 
+{
+    if (isLoggable(LOG_LEVEL_DEBUG)) {
+        PLATFORM_VA_LIST argList;
+        PLATFORM_VA_START (argList, msg);
+        printMessage(LOG_DEBUG, msg, argList);
+        PLATFORM_VA_END(argList);
+    }
+}
+void SymbianLog::developer(const char*  msg, ...) 
+{
+    if (isLoggable(LOG_LEVEL_INFO)) {
+        PLATFORM_VA_LIST argList;
+        PLATFORM_VA_START (argList, msg);
+        printMessage(LOG_DEBUG, msg, argList);
+        PLATFORM_VA_END(argList);
+    }
+}
+
+void SymbianLog::printMessage(const char* level, const char* msg, PLATFORM_VA_LIST argList) 
+{
+    iSemaphore.Wait();
+    
+    StringBuffer currentTime = createCurrentTime(false);
+    
+    TInt err = file.Open(fsSession, iLogName, EFileWrite|EFileShareAny);
+    TInt pos = 0;
+    
+    if (err == KErrNotFound) 
+    {
+        // First time: file does not exist. Create it.
+        err = file.Create(fsSession, iLogName, EFileWrite|EFileShareAny);
+        if (err != KErrNone) {
+            setErrorF(err, "SymbianLog: could not open log file (code %d)", err);
+            goto finally;
+        }
+        StringBuffer header = createHeader();
+        RBuf8 data;
+        data.Assign(stringBufferToNewBuf8(header));
+        file.Write(data);
+        data.Close();
+    }
+    else 
+    {
+        err = file.Seek(ESeekEnd, pos);
+        if (err != KErrNone) {
+            setErrorF(err, "SymbianLog: seek error on log file (code %d)", err);
+            goto finally;
+        }
+    }
+
+    {
+        // Write the data
+        StringBuffer line, data;
+        line.sprintf("%s -%s- %s", currentTime.c_str(), level, msg);
+        data.vsprintf(line.c_str(), argList);
+        data.append("\n");
+        
+        RBuf8 buf;
+        buf.Assign(stringBufferToNewBuf8(data));
+        file.Write(buf);
+        buf.Close();
+    }
+    
+finally:
+    file.Close();
+    // we need closed file to operate on it
+    if ( LogSize() > SYMBIAN_LOG_SIZE ){
+        // roll log file
+        RollLogFile();
+    }
+    iSemaphore.Signal();
+}
+
+void SymbianLog::reset(const char* title) 
+{
+    iSemaphore.Wait();
+    
+    TInt err = file.Replace(fsSession, iLogName, EFileWrite|EFileShareAny);
+    if (err != KErrNone) {
+        setErrorF(err, "SymbianLog: error resetting the log file (code %d)", err);
+        return;
+    }
+    
+    // Write the Header
+    StringBuffer header = createHeader(title);
+    RBuf8 buf;
+    buf.Assign(stringBufferToNewBuf8(header));
+    file.Write(buf);
+    buf.Close();
+    
+    file.Close();
+    iSemaphore.Signal();
+}
+
+
+size_t SymbianLog::getLogSize() 
+{
+    return (size_t)LogSize();
+}
+
+TInt SymbianLog::LogSize()
+{
+    TEntry entry;
+    fsSession.Entry(iLogName,entry);
+    TInt size = entry.iSize;
+
+    return size;
+}
+
+void SymbianLog::RollLogFile(void)
+{
+    CFileMan* fileMan = CFileMan::NewL(fsSession);
+    CleanupStack::PushL(fileMan);
+    //copy the current file into the roll file, file must be open
+    TInt err = KErrNone;
+    err = file.Open(fsSession, iLogName, EFileWrite|EFileShareAny);
+    if (err != KErrNone) {
+        setErrorF(err, "SymbianLog: could not open log file (code %d)", err);
+    } else {
+        err = fileMan->Copy(file,iRollLogName,CFileMan::EOverWrite);
+    }
+    if (err != KErrNone) {
+        setErrorF(err, "SymbianLog: copy error on roll log file (code %d)", err);
+    }
+    file.Close();
+    // reset the current file
+    // we don't use reset() method because we don't want 
+    // nested semaphores
+    err = KErrNone;
+    err = file.Replace(fsSession, iLogName, EFileWrite|EFileShareAny);
+    if (err != KErrNone) {
+        setErrorF(err, "SymbianLog: error resetting the log file (code %d)", err);
+        return;
+    }
+    // Write the Header
+    StringBuffer header = createHeader();
+    RBuf8 buf;
+    buf.Assign(stringBufferToNewBuf8(header));
+    file.Write(buf);
+    buf.Close();
+    file.Close();
+    
+    CleanupStack::PopAndDestroy(fileMan);
+
+}
+
+Log *Log::logger;
+
+Log &Log::instance() {
+    if (!logger) {
+        logger = new SymbianLog();
+    }
+    return *logger;
+}
