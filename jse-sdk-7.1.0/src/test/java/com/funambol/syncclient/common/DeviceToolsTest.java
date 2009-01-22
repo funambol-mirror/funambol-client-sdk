571d26328f7f9afaf2d73021a4376e2ee5680a4c
dimuro <dimuro@3966be30-755a-0410-815e-dd79f492ac3c>
2009-01-22 09:54:01 +0000

    [maven-release-plugin]  copy for tag jse-sdk-7.1.0
    git-svn-id: https://outlook-client.forge.funambol.org/svn/client-sdk/tags/jse-sdk@328 3966be30-755a-0410-815e-dd79f492ac3c

---
+++ jse-sdk-7.1.0/src/test/java/com/funambol/syncclient/common/DeviceToolsTest.java
@@ -0,0 +1,103 @@
+/*
+ * Funambol is a mobile platform developed by Funambol, Inc.
+ * Copyright (C) 2008 Funambol, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation with the addition of the following permission
+ * added to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED
+ * WORK IN WHICH THE COPYRIGHT IS OWNED BY FUNAMBOL, FUNAMBOL DISCLAIMS THE
+ * WARRANTY OF NON INFRINGEMENT  OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
+ *
+ * You can contact Funambol, Inc. headquarters at 643 Bair Island Road, Suite
+ * 305, Redwood City, CA 94063, USA, or at email address info@funambol.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * "Powered by Funambol" logo. If the display of the logo is not reasonably
+ * feasible for technical reasons, the Appropriate Legal Notices must display
+ * the words "Powered by Funambol".
+ */
+package com.funambol.syncclient.common;
+
+import junit.framework.TestCase;
+import junitx.util.PrivateAccessor;
+
+/**
+ *
+ * @author nichele
+ */
+public class DeviceToolsTest extends TestCase {
+
+    // ------------------------------------------------------------ Constructors
+    public DeviceToolsTest(String testName) {
+        super(testName);
+    }
+
+    // -------------------------------------------------------------- Test cases
+
+    /**
+     * Test of computeDeviceID.
+     */
+    public void testComputeDeviceID() throws Throwable {
+
+        DeviceTools deviceTools = new DeviceTools();
+
+        String prefix       = "fjg-";
+
+        String result = (String)PrivateAccessor.invoke(deviceTools,
+                                                       "computeDeviceID",
+                                                       new Class[] {String.class,
+                                                                    String.class,
+                                                                    String.class,
+                                                                    String.class},
+                                                       new Object[] {"fjg-",
+                                                                     "username",
+                                                                     "hostName",
+                                                                     "hostAddress"});
+        String expResult = "fjg-k/BXenzvzhzEhqbr2LRvAw==";
+
+        assertEquals(expResult, result);
+
+        result = (String)PrivateAccessor.invoke(deviceTools,
+                                                       "computeDeviceID",
+                                                       new Class[] {String.class,
+                                                                    String.class,
+                                                                    String.class,
+                                                                    String.class},
+                                                       new Object[] {"fjg-",
+                                                                     "longUsernameAAAAAAAAAAAAA",
+                                                                     "longHostNameAAAAAAAAAAAAA",
+                                                                     "longHostAddressBBBBBBBBBB"});
+
+        expResult = "fjg-tT/sMdKjVjCY2o3qNHi8sw==";
+
+        assertEquals(expResult, result);
+    }
+
+    // ------------------------------------------------------- Protected methods
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+}
