a59b485e4c695b1dd3c9063ff7398f16a352c52d
carlo <carlo@3966be30-755a-0410-815e-dd79f492ac3c>
2011-02-09 10:11:01 +0000

    Added JSONSyncSource
    git-svn-id: https://outlook-client.forge.funambol.org/svn/client-sdk/branches/java-sdk@3315 3966be30-755a-0410-815e-dd79f492ac3c

---
+++ us7638/sapisync/src/main/java/com/funambol/sapisync/source/JSONFileObject.java
@@ -0,0 +1,121 @@
+/*
+ * Funambol is a mobile platform developed by Funambol, Inc. 
+ * Copyright (C) 2008 Funambol, Inc.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation with the addition of the following permission 
+ * added to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED
+ * WORK IN WHICH THE COPYRIGHT IS OWNED BY FUNAMBOL, FUNAMBOL DISCLAIMS THE 
+ * WARRANTY OF NON INFRINGEMENT  OF THIRD PARTY RIGHTS.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License 
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
+ * 
+ * You can contact Funambol, Inc. headquarters at 643 Bair Island Road, Suite 
+ * 305, Redwood City, CA 94063, USA, or at email address info@funambol.com.
+ * 
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ * 
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * "Powered by Funambol" logo. If the display of the logo is not reasonably 
+ * feasible for technical reasons, the Appropriate Legal Notices must display
+ * the words "Powered by Funambol". 
+ */
+
+package com.funambol.sapisync.source;
+
+import java.util.Vector;
+import org.json.me.JSONArray;
+
+import org.json.me.JSONException;
+import org.json.me.JSONObject;
+
+/**
+ * Represents a JSON object containing a file meta data. It can also contain a
+ * Vector of JSONFileThumbnail objects.
+ */
+public class JSONFileObject extends JSONObject {
+
+    private String id;
+    private String name;
+    private String url;
+    
+    private long date;
+    private long size;
+
+    private Vector thumbnails;
+
+    public JSONFileObject(String json) throws JSONException {
+        super(json);
+
+        this.id   = getString("id");
+        this.name = getString("name");
+        this.url  = getString("url");
+
+        this.date = getLong("date");
+        this.size = getLong("size");
+
+        JSONArray thumbs = getJSONArray("thumbnails");
+        if(thumbs != null) {
+            this.thumbnails = new Vector();
+            for(int i=0; i<thumbs.length(); i++) {
+                JSONObject thumb = thumbs.getJSONObject(i);
+                this.thumbnails.addElement(new JSONFileThumbnail(thumb));
+            }
+        }
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public long getDate() {
+        return date;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public Vector getThumbnails() {
+        return thumbnails;
+    }
+
+    public class JSONFileThumbnail {
+
+        private String size;
+        private String url;
+
+        public JSONFileThumbnail(JSONObject json) throws JSONException {
+            this.size = json.getString("size");
+            this.url  = getString("url");
+        }
+
+        public String getSize() {
+            return size;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+    }
+}
---
+++ us7638/sapisync/src/main/java/com/funambol/sapisync/source/JSONSyncSource.java
@@ -0,0 +1,199 @@
+/*
+ * Funambol is a mobile platform developed by Funambol, Inc. 
+ * Copyright (C) 2008 Funambol, Inc.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation with the addition of the following permission 
+ * added to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED
+ * WORK IN WHICH THE COPYRIGHT IS OWNED BY FUNAMBOL, FUNAMBOL DISCLAIMS THE 
+ * WARRANTY OF NON INFRINGEMENT  OF THIRD PARTY RIGHTS.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License 
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
+ * 
+ * You can contact Funambol, Inc. headquarters at 643 Bair Island Road, Suite 
+ * 305, Redwood City, CA 94063, USA, or at email address info@funambol.com.
+ * 
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ * 
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * "Powered by Funambol" logo. If the display of the logo is not reasonably 
+ * feasible for technical reasons, the Appropriate Legal Notices must display
+ * the words "Powered by Funambol". 
+ */
+
+package com.funambol.sapisync.source;
+
+import com.funambol.sync.SyncItem;
+import com.funambol.sync.SourceConfig;
+import com.funambol.sync.SyncException;
+import com.funambol.sync.SyncSource;
+import com.funambol.sync.client.ChangesTracker;
+import com.funambol.sync.client.TrackableSyncSource;
+import com.funambol.platform.HttpConnectionAdapter;
+import com.funambol.util.ConnectionManager;
+import com.funambol.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Represents a SyncSource which handles JSON file objects as input SyncItems.
+ * You should define the getDownloadOutputStream in order to provide the
+ * OutputSteam used to download the file.
+ */
+public abstract class JSONSyncSource extends TrackableSyncSource {
+
+    private static final String TAG_LOG = "JSONSyncSource";
+
+    protected boolean downloadFileObject;
+    protected boolean downloadThumbnails;
+
+    //------------------------------------------------------------- Constructors
+
+    /**
+     * JSONSyncSource constructor: initialize source config
+     */
+    public JSONSyncSource(SourceConfig config, ChangesTracker tracker) {
+        super(config, tracker);
+        downloadFileObject = true;
+        downloadThumbnails = false;
+    }
+
+    public int addItem(SyncItem item) throws SyncException {
+        super.addItem(item);
+        return addUpdateItem(item, false);
+    }
+
+    public int updateItem(SyncItem item) throws SyncException {
+        super.updateItem(item);
+        return addUpdateItem(item, true);
+    }
+
+    private int addUpdateItem(SyncItem item, boolean isUpdate) throws SyncException {
+        try {
+            String itemContent = new String(item.getContent());
+            JSONFileObject jsonFile = new JSONFileObject(itemContent);
+            if(downloadFileObject) {
+                String name    = jsonFile.getName();
+                String baseUrl = jsonFile.getUrl();
+                long size      = jsonFile.getSize();
+                OutputStream fileos = getDownloadOutputStream(jsonFile, isUpdate, false);
+                downloadToOutputStream(composeUrl(baseUrl, name), fileos, size);
+            }
+            if(downloadThumbnails) {
+                // TODO FIXME download thumbnails
+            }
+            return SyncSource.STATUS_SUCCESS;
+        } catch (Throwable t) {
+            Log.error(TAG_LOG, "Cannot save json item", t);
+            return SyncSource.STATUS_RECV_ERROR;
+        }
+    }
+
+    protected OutputStream getDownloadOutputStream(JSONFileObject jsonItem,
+            boolean isUpdate, boolean isThumbnail) {
+        return getDownloadOutputStream(
+                jsonItem.getName(),
+                jsonItem.getSize(),
+                isUpdate,
+                isThumbnail);
+    }
+
+    /**
+     * Must be implemented in order to provide a proper OutputStream to download
+     * the item.
+     * 
+     * @param name
+     * @param size
+     * @param isUpdate
+     * @param isThumbnail
+     * @return
+     */
+    protected abstract OutputStream getDownloadOutputStream(String name,
+            long size, boolean isUpdate, boolean isThumbnail);
+
+    /**
+     * Download the given url to the given OutputStream.
+     * 
+     * @param url
+     * @param os
+     * @param size
+     */
+    private void downloadToOutputStream(String url, OutputStream os, long size) {
+
+        HttpConnectionAdapter conn = null;
+        InputStream is = null;
+        try {
+            if (Log.isLoggable(Log.DEBUG)) {
+                Log.debug(TAG_LOG, "Sending http request to: " + url);
+            }
+            conn = ConnectionManager.getInstance().openHttpConnection(
+                    url, "wrapper");
+            conn.setRequestMethod(HttpConnectionAdapter.GET);
+            if (Log.isLoggable(Log.DEBUG)) {
+                Log.debug(TAG_LOG, "Response is: " + conn.getResponseCode());
+            }
+            if (conn.getResponseCode() == HttpConnectionAdapter.HTTP_OK) {
+                is = conn.openInputStream();
+                // Read until we have data
+                int b;
+                do {
+                    b = is.read();
+                    size--;
+                    if (b != -1) {
+                        os.write(b);
+                    }
+                } while(b != -1 && size > 0);
+            } else {
+                Log.error(TAG_LOG, "Http request failed. Server replied: " +
+                        conn.getResponseCode() + ", message: " +
+                        conn.getResponseMessage());
+                return;
+            }
+        } catch(IOException ex) {
+            Log.error(TAG_LOG, "Http download failed", ex);
+            return;
+        } finally {
+            // Release all resources
+            if (os != null) {
+                try {
+                    os.close();
+                } catch (IOException e) {}
+                os = null;
+            }
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {}
+                is = null;
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (IOException e) {}
+                conn = null;
+            }
+        }
+    }
+
+    private String composeUrl(String baseUrl, String filename) {
+        StringBuffer res = new StringBuffer();
+        res.append(baseUrl);
+        res.append('/');
+        res.append(filename);
+        return res.toString();
+    }
+}
