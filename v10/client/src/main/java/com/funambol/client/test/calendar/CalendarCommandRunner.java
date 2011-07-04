733643136b55fb2c3a62d2d0f88aed9f6b8439c1
garatti <garatti@3966be30-755a-0410-815e-dd79f492ac3c>
2011-07-04 10:24:39 +0000

    Branch for v10
    git-svn-id: https://outlook-client.forge.funambol.org/svn/client-sdk/branches/java-sdk@4312 3966be30-755a-0410-815e-dd79f492ac3c

---
+++ v10/client/src/main/java/com/funambol/client/test/calendar/CalendarCommandRunner.java
@@ -0,0 +1,261 @@
+/*
+ * Funambol is a mobile platform developed by Funambol, Inc.
+ * Copyright (C) 2010 Funambol, Inc.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation with the addition of the following permission
+ * added to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED
+ * WORK IN WHICH THE COPYRIGHT IS OWNED BY FUNAMBOL, FUNAMBOL DISCLAIMS THE
+ * WARRANTY OF NON INFRINGEMENT  OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
+ * 
+ * You can contact Funambol, Inc. headquarters at 643 Bair Island Road, Suite
+ * 305, Redwood City, CA 94063, USA, or at email address info@funambol.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * "Powered by Funambol" logo. If the display of the logo is not reasonably
+ * feasible for technical reasons, the Appropriate Legal Notices must display
+ * the words "Powered by Funambol".
+ */
+
+package com.funambol.client.test.calendar;
+
+import java.util.Vector;
+
+import com.funambol.client.test.CommandRunner;
+
+
+public class CalendarCommandRunner extends CommandRunner implements EventsUserCommands {
+
+    private static final String TAG_LOG = "CalendarCommandRunner";
+
+    // Event fields used by SetEventField and EmptyEventField commands
+    public static final String EVENT_FIELD_SUMMARY      = "Summary";
+    public static final String EVENT_FIELD_LOCATION     = "Location";
+    public static final String EVENT_FIELD_START        = "Start";
+    public static final String EVENT_FIELD_END          = "End";
+    public static final String EVENT_FIELD_ALLDAY       = "AllDay";
+    public static final String EVENT_FIELD_DESCRIPTION  = "Description";
+    public static final String EVENT_FIELD_ATTENDEES    = "Attendees";
+    public static final String EVENT_FIELD_TIMEZONE     = "Timezone";
+    public static final String EVENT_FIELD_REMINDER     = "Reminder";
+    public static final String EVENT_FIELD_DURATION     = "Duration";
+
+    public CalendarCommandRunner(CalendarRobot robot) {
+        super(robot);
+    }
+
+    public boolean runCommand(String command, Vector pars) throws Throwable {
+        if (CREATE_EMPTY_EVENT_COMMAND.equals(command)) {
+            createEmptyEvent(command, pars);
+        } else if (LOAD_EVENT_COMMAND.equals(command)) {
+            loadEvent(command, pars);
+        } else if (SET_EVENT_FIELD_COMMAND.equals(command)) {
+            setEventField(command, pars);
+        } else if (EMPTY_EVENT_FIELD_COMMAND.equals(command)) {
+            emptyEventField(command, pars);
+        } else if (SAVE_EVENT_COMMAND.equals(command)) {
+            saveEvent(command, pars);
+        } else if (DELETE_EVENT_COMMAND.equals(command)) {
+            deleteEvent(command, pars);
+        } else if (DELETE_ALL_EVENTS_COMMAND.equals(command)) {
+            deleteAllEvents(command, pars);
+        } else if (SAVE_EVENT_ON_SERVER_COMMAND.equals(command)) {
+            saveEventOnServer(command, pars);
+        } else if (DELETE_EVENT_ON_SERVER_COMMAND.equals(command)) {
+            deleteEventOnServer(command, pars);
+        } else if (DELETE_ALL_EVENTS_ON_SERVER_COMMAND.equals(command)) {
+            deleteAllEventsOnServer(command, pars);
+        } else if (SET_EVENT_AS_VCAL_COMMAND.equals(command)){
+            setEventAsVCal(command, pars);
+        } else if (SET_EVENT_FROM_SERVER.equals(command)) {
+            setEventFromServer(command, pars);
+        } else if (CREATE_EMPTY_RAW_EVENT.equals(command)) {
+            createEmptyRawEvent(command, pars);
+        } else if (SET_RAW_EVENT_FIELD.equals(command)) {
+            setRawEventField(command, pars);
+        } else if (SAVE_RAW_EVENT.equals(command)) {
+            saveRawEvent(command, pars);
+        } else if (CHECK_RAW_EVENT_AS_VCAL.equals(command)) {
+            checkRawEventAsVCal(command, pars);
+        } else if (SET_RAW_REMINDER_FIELD.equals(command)) {
+            setRawReminderField(command, pars);
+        } else if (CHECK_RAW_EVENT_FIELD.equals(command)) {
+            checkRawEventField(command, pars);
+        } else if (CHECK_RAW_REMINDER_FIELD.equals(command)) {
+            checkRawReminderField(command, pars);
+        } else if (CHECK_EVENTS_COUNT_ON_SERVER_COMMAND.equals(command)) {
+            checkEventsCountOnServer(command, pars);
+        } else if (CHECK_EVENTS_COUNT_COMMAND.equals(command)) {
+            checkEventsCount(command, pars);
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+    private CalendarRobot getCalendarRobot() {
+        return (CalendarRobot)robot;
+    }
+
+    private void createEmptyEvent(String command, Vector args) throws Throwable {
+        getCalendarRobot().createEmptyEvent();
+    }
+
+    private void setEventField(String command, Vector args) throws Throwable {
+        String field = getParameter(args, 0);
+        String value = getParameter(args, 1);
+        checkArgument(field, "Missing field name in " + command);
+        checkArgument(value, "Missing value in " + command);
+        getCalendarRobot().setEventField(field, value);
+    }
+
+    private void emptyEventField(String command, Vector args) throws Throwable {
+        String field = getParameter(args, 0);
+        checkArgument(field, "Missing field in " + command);
+        getCalendarRobot().setEventField(field, "");
+    }
+
+    private void loadEvent(String command, Vector args) throws Throwable {
+        String summary = getParameter(args, 0);
+        checkArgument(summary, "Missing summary in " + command);
+        getCalendarRobot().loadEvent(summary);
+    }
+
+    private void saveEvent(String command, Vector args) throws Throwable {
+        String useNativeApp = getParameter(args, 0);
+        if("true".equals(useNativeApp)) {
+            getCalendarRobot().saveEvent(true);
+        } else {
+            getCalendarRobot().saveEvent();
+        }
+    }
+
+    private void deleteEvent(String command, Vector args) throws Throwable {
+        String summary = getParameter(args, 0);
+        checkArgument(summary, "Missing summary in " + command);
+        getCalendarRobot().deleteEvent(summary);
+    }
+
+    private void deleteAllEvents(String command, Vector args) throws Throwable {
+        getCalendarRobot().deleteAllEvents();
+    }
+
+    private void saveEventOnServer(String command, Vector args) throws Throwable {
+        String summary = getParameter(args, 0);
+        checkArgument(summary, "Missing summary in " + command);
+        getCalendarRobot().saveEventOnServer(summary);
+    }
+
+    private void deleteEventOnServer(String command, Vector args) throws Throwable {
+        String summary = getParameter(args, 0);
+        checkArgument(summary, "Missing summary in " + command);
+        getCalendarRobot().deleteEventOnServer(summary);
+    }
+
+    private void deleteAllEventsOnServer(String command, Vector args) throws Throwable {
+        getCalendarRobot().deleteAllEventsOnServer();
+    }
+
+    private void setEventAsVCal(String command, Vector args) throws Throwable {
+        String vcal = getParameter(args, 0);
+        checkArgument(vcal, "Missing vcal in " + command);
+        getCalendarRobot().setEventAsVCal(vcal);
+    }
+
+    private void setEventFromServer(String command, Vector args) throws Throwable {
+        String vcal = getParameter(args, 0);
+        checkArgument(vcal, "Missing vcal in " + command);
+        getCalendarRobot().setEventFromServer(vcal);
+    }
+
+    private void createEmptyRawEvent(String command, Vector args) throws Throwable {
+        getCalendarRobot().createEmptyRawEvent();
+    }
+
+    private void setRawEventField(String command, Vector args) throws Throwable {
+        String fieldName  = getParameter(args, 0);
+        String fieldValue = getParameter(args, 1);
+        checkArgument(fieldName,  "Missing field name in " + command);
+        checkArgument(fieldValue, "Missing field value in " + command);
+        getCalendarRobot().setRawEventField(fieldName, fieldValue);
+    }
+
+    private void setRawReminderField(String command, Vector args) throws Throwable {
+        String fieldName  = getParameter(args, 0);
+        String fieldValue = getParameter(args, 1);
+        checkArgument(fieldName,  "Missing field name in " + command);
+        checkArgument(fieldValue, "Missing field value in " + command);
+        getCalendarRobot().setRawReminderField(fieldName, fieldValue);
+    }
+
+    private void saveRawEvent(String command, Vector args) throws Throwable {
+        getCalendarRobot().saveRawEvent();
+    }
+
+    private void checkRawEventAsVCal(String command, Vector args) throws Throwable {
+        String vcal = getParameter(args, 0);
+        checkArgument(vcal, "Missing vcal in " + command);
+        getCalendarRobot().checkRawEventAsVCal(vcal);
+    }
+
+    private void checkRawReminderField(String command, Vector args) throws Throwable {
+        String fieldName  = getParameter(args, 0);
+        String fieldValue = getParameter(args, 1);
+        checkArgument(fieldName,  "Missing field name in " + command);
+        checkArgument(fieldValue, "Missing field value in " + command);
+        getCalendarRobot().checkRawReminderField(fieldName, fieldValue);
+    }
+
+    private void checkRawEventField(String command, Vector args) throws Throwable {
+        String fieldName  = getParameter(args, 0);
+        String fieldValue = getParameter(args, 1);
+        checkArgument(fieldName,  "Missing field name in " + command);
+        checkArgument(fieldValue, "Missing field value in " + command);
+        getCalendarRobot().checkRawEventField(fieldName, fieldValue);
+    }
+
+    /**
+     * Command to check the items count on server
+     * @param command the String formatted command to check the server's items
+     * count
+     * @param args the command's String formatted arguments
+     * @throws Throwable if anything went wrong
+     */
+    private void checkEventsCountOnServer(String command, Vector args) throws Throwable {
+        String count =  getParameter(args, 0);
+        checkArgument(count, "Missing count in " + command);
+        getCalendarRobot().checkItemsCountOnServer(Integer.parseInt(count));
+    }
+
+        /**
+     * Command to check the items count on the device
+     * @param command the String formatted command to check the client's items
+     * count
+     * @param args the command's String formatted arguments
+     * @throws Throwable if anything went wrong
+     */
+    public void checkEventsCount(String command, Vector args) throws Throwable {
+        String count =  getParameter(args, 0);
+        checkArgument(count, "Missing count in " + command);
+        getCalendarRobot().checkEventsCount(Integer.parseInt(count));
+    }
+
+
+}
+ 
---
+++ v10/sync/src/main/java/com/funambol/sync/client/ChangesTracker.java
@@ -0,0 +1,210 @@
+/*
+ * Funambol is a mobile platform developed by Funambol, Inc.
+ * Copyright (C) 2008 Funambol, Inc.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation with the addition of the following permission
+ * added to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED
+ * WORK IN WHICH THE COPYRIGHT IS OWNED BY FUNAMBOL, FUNAMBOL DISCLAIMS THE
+ * WARRANTY OF NON INFRINGEMENT  OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
+ * 
+ * You can contact Funambol, Inc. headquarters at 643 Bair Island Road, Suite
+ * 305, Redwood City, CA 94063, USA, or at email address info@funambol.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * "Powered by Funambol" logo. If the display of the logo is not reasonably
+ * feasible for technical reasons, the Appropriate Legal Notices must display
+ * the words "Powered by Funambol".
+ */
+
+package com.funambol.sync.client;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+import com.funambol.sync.SyncItem;
+
+/**
+ * This interface can be used by TrackableSyncSource to detect changes occourred
+ * since the last synchronization. The API provides a basic implementation
+ * in CacheTracker which detects changes comparing fingerprints.
+ * Client can implement this interface and use it in the TrackableSyncSource if more
+ * efficient methods are available.
+ */
+public interface ChangesTracker {
+
+    /**
+     * This method associates this tracker to the given Sync Source. This method
+     * must be called once before using the tracker. The tracker is invalid
+     * until this method is invoked.
+     */
+    public void setSyncSource(TrackableSyncSource ss);
+
+    /**
+     * This method allows implementations to get ready to return list of
+     * changes. An implementation may decide to detect all changes at this point
+     * or not, the interface does not enforce any semantics on this.
+     * The only requirement is that any change made before this method is
+     * invoked must be part of the lists returned by other methods.
+     *
+     * @param syncMode The sync mode
+     * @param resume true if this sync is resuming a previously interrupted one
+     */
+    public void begin(int syncMode, boolean resume) throws TrackerException;
+
+    /**
+     * This method allows implementations to clean data. After this method is
+     * invoked it is illegal to fetch list of changes. A new call to "begin" is
+     * required first.
+     */
+    public void end() throws TrackerException;
+
+    /**
+     * Returns the list of new items. The list of new items is frozen when the
+     * method is invoked, after this method returns any change is not part of
+     * the list of new items returned. Changes that happen after this method
+     * returns will be part of the next changes set.
+     * There is no guarantee that the list will contain all the changes that
+     * happened after the begin method was invoked. This is an implementation
+     * choice, it is only guaranteed that the list includes all the changes at
+     * the time "begin" was invoked.
+     *
+     * @return the list of new items as an Enumeration
+     *         of SyncItem
+     */
+    public Enumeration getNewItems() throws TrackerException;
+
+    /**
+     * Returns the number of new items that will be returned by the getNewItems
+     * method
+     *
+     * @return the number of items
+     */
+    public int getNewItemsCount() throws TrackerException;
+
+    /**
+     * Returns the list of updated items. The list of updated items is frozen when the
+     * method is invoked, after this method returns any change is not part of
+     * the list of new items returned. Changes that happen after this method
+     * returns will be part of the next changes set.
+     * There is no guarantee that the list will contain all the changes that
+     * happened after the begin method was invoked. This is an implementation
+     * choice, it is only guaranteed that the list includes all the changes at
+     * the time "begin" was invoked.
+     *
+     * @return the list of updated items as an Enumeration
+     *         of SyncItem
+     */
+    public Enumeration getUpdatedItems() throws TrackerException;
+
+    /**
+     * Returns the number of updated items that will be returned by the getUpdatedItems
+     * method
+     *
+     * @return the number of items
+     */
+    public int getUpdatedItemsCount() throws TrackerException;
+
+
+    /**
+     * Returns the list of deleted items. The list of deleted items is frozen when the
+     * method is invoked, after this method returns any change is not part of
+     * the list of new items returned. Changes that happen after this method
+     * returns will be part of the next changes set.
+     * There is no guarantee that the list will contain all the changes that
+     * happened after the begin method was invoked. This is an implementation
+     * choice, it is only guaranteed that the list includes all the changes at
+     * the time "begin" was invoked.
+ 
+     *
+     * @return the list of updated items as an Enumeration
+     *         of strings (SyncItem's keys)
+     */
+    public Enumeration getDeletedItems() throws TrackerException;
+
+    /**
+     * Returns the number of deleted items that will be returned by the getDeletedItems
+     * method
+     *
+     * @return the number of items
+     */
+    public int getDeletedItemsCount() throws TrackerException;
+
+    /**
+     * Set the status for a group of items. The status is returned by the server as result of the
+     * synchronization. This method is needed by the tracker to decide
+     * if an items can be removed from the list of changes or they must be kept
+     * for the next run
+     *
+     * @param key the item key (cannot be null)
+     * @param status the syncml status for this item
+     */
+    public void setItemsStatus(Vector itemsStatus) throws TrackerException;
+
+    /**
+     * This method resets the list of changes. The lists (new, upd, del) will be empty
+     * immediately after this call is invoked.
+     */
+    public void reset() throws TrackerException;
+
+    /**
+     * Remove the given item from the list of changes
+     *
+     * @param item is the item to be removed
+     * @return true if an item was found and deleted
+     */
+    public boolean removeItem(SyncItem item) throws TrackerException;
+
+    /**
+     * Empty the tracker status. After this method is invoked all the items
+     * currently present will be reported as new items.
+     */
+    public void empty() throws TrackerException;
+
+    /**
+     * Returns true if this tracker supports sync resume.
+     */
+    public boolean supportsResume();
+
+    /**
+     * Checks if the given item has changed since the beginning of the last sync
+     *
+     * @param key the item key
+     * @param ts the last sync start time
+     *
+     * @return true if the item has changed since the last sync
+     */
+    public boolean hasChangedSinceLastSync(String key, long ts);
+    
+    
+    /**
+     * Checks if an item should be filtered out or not.
+     * Typical implementation will call the {@link TrackableSyncSource#filterOutgoingItem(String)}
+     * method, leaving to SyncSource all the filtering logic.
+     * Simply return false if tracker (and linked SyncSource) don't implement
+     * any filters.
+     * 
+     * @param key
+     * @param removed specifies if this item was removed from local storage
+     * @return true if the item must be filtered out, otherwise false
+     */
+    public boolean filterItem(String key, boolean removed);
+
+}
+
